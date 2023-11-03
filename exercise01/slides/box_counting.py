a14cd6060d660e0a4b161fd98a0c822f85008889
adrian <s6adkoer@uni-bonn.de>
2023-11-03 11:01:06 +0100

    Format box_counting s.t it fits on the slide

--- exercise01/slides/box_counting.py
+++ exercise01/slides/box_counting.py
@@ -2,20 +2,17 @@ def box_counting(img):
     w, h = img.shape
     n_ls = []
     # l runs from 1 to 9-2 =7
-    for l in range(1,8):
-        s_l = 1/2**l
-        # get box sizes
-        box_sizeW = s_l * w
-        box_sizeH = s_l * h
-        n_l=0
-        #each l has 2**l boxes
-        for box_w in range(0,(2**l)):
+    for l in range(1, 8):
+        n_l, s_l = 0, 1/2**l                        # setup counter and scale
+        box_sizeW, box_sizeH = s_l * w, s_l * h     # get box sizes
+
+        for box_w in range(0,(2**l)):               # each l has 2**l boxes
             for box_h in range(0,(2**l)):
                 #check if any value in the box is equal 1.
                 #If so increment n_l by one
-                if (np.any(img[int(box_w * box_sizeW):int((box_w+1) * box_sizeW),\
+                if (np.any(img[int(box_w * box_sizeW): int((box_w+1) * box_sizeW),\
                                int(box_h * box_sizeH): int((box_h+1) * box_sizeH)]\
                             ==1)):
                     n_l+=1
         n_ls.append(n_l)
-    return n_l
+    return n_ls
