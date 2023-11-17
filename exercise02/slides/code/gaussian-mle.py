55b70417d5b21c85dd73ee9811d55ef49db33922
Adrian Köring <s6adkoer@uni-bonn.de>
2023-11-17 10:16:03 +0100

    Reword some slides

--- exercise02/slides/code/gaussian-mle.py
+++ exercise02/slides/code/gaussian-mle.py
@@ -1,5 +1,5 @@
 µ = np.mean(X, axis=1, keepdims=True)
-# µ = [[175.729], [73.865]]
+# µ = [[175.729], [73.865]] with µ.shape=[2, 1]
 S = np.cov(X - µ, ddof=1) # ddof = degree of freedom = scales with 1/(n-1)
 # S == (1 / (N-1)) * (X - µ) @ (X - µ).T
 # S == [[ 75.925,  64.546 ],
